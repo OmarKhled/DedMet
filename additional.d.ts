57130e071cd2c55ce94be0c7d464400fb0326bd8
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-04-22 01:41:45 +0200

    Add additional.d.ts

---
+++ additional.d.ts
@@ -0,0 +1,5 @@
+export as namespace JSX;
+
+export interface IntrinsicElements {
+  "lottie-player": any;
+}
--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-export as namespace JSX;
-
-export interface IntrinsicElements {
-  "lottie-player": any;
-}
-
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
