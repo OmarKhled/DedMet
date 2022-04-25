0d290c4a9f4bf3b56c2f5dd116206dba4732c9db
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-04-25 17:15:46 +0200

    Build Error fox

--- components/SubscriptionFrom/PaymentDetails.tsx
+++ components/SubscriptionFrom/PaymentDetails.tsx
@@ -5,17 +5,17 @@ const PaymentDetails: FC = () => {
   const { getValues } = useFormContext();
 
   useEffect(() => {
-    // Paddle.Checkout.open({
-    //   method: "inline",
-    //   product: 768379,
-    //   allowQuantity: false,
-    //   disableLogout: true,
-    //   frameTarget: "paymentDetails",
-    //   frameInitialHeight: 416,
-    //   frameStyle:
-    //     "width:100%; min-width:312px; background-color: transparent; border: none;",
-    // });
-    Paddle.Checkout.open({ product: 768379 });
+    Paddle.Checkout.open({
+      method: "inline",
+      product: 768379,
+      allowQuantity: false,
+      disableLogout: true,
+      frameTarget: "paymentDetails",
+      frameInitialHeight: 416,
+      frameStyle:
+        "width:100%; min-width:312px; background-color: transparent; border: none;",
+    });
+    // Paddle.Checkout.open({ product: 768379 });
   }, []);
 
   return (
