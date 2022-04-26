03f89d4800b82dbf63549e685854e736cc63d85c
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-04-26 08:51:56 +0200

    Payment Details loading spinner

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from "react";
 
-const LoadingSpinner: FC = () => {
+const LoadingSpinner: FC<{ className?: string }> = ({ className }) => {
   return (
-    <div className="spinner">
+    <div className={`spinner ${className}`}>
       <div className="dot1"></div>
       <div className="dot2"></div>
     </div>
   );
+};
+
+LoadingSpinner.defaultProps = {
+  className: "",
 };
 
 export default LoadingSpinner;
--- components/SubscriptionFrom/PaymentDetails.tsx
+++ components/SubscriptionFrom/PaymentDetails.tsx
@@ -1,8 +1,10 @@
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { useFormContext } from "react-hook-form";
+import LoadingSpinner from "../LoadingSpinner";
 
 const PaymentDetails: FC = () => {
   const { getValues } = useFormContext();
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const id = Number(process.env.NEXT_PUBLIC_DEDMET_PRODUCT_ID);
@@ -17,6 +19,9 @@ const PaymentDetails: FC = () => {
       email: getValues("email"),
       country: "EG",
       passthrough: JSON.stringify(getValues()),
+      loadCallback: () => {
+        setLoading(false);
+      },
     });
   }, []);
 
@@ -26,8 +31,12 @@ const PaymentDetails: FC = () => {
         <div className="head">
           <p>Payment Details</p>
         </div>
-        <div className="body">
-          <div className="paymentDetails"></div>
+        <div
+          className={`body ${loading ? "d-flex align-items-center" : ""}`}
+          style={loading ? { height: "416px" } : {}}
+        >
+          {loading && <LoadingSpinner className="m-auto d-block" />}
+          <div className={`paymentDetails${loading ? " d-none" : ""}`}></div>
         </div>
       </div>
     </>
