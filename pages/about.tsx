dad3ef8a37880b17967ad2b0300df3dc63f8170e
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2022-04-28 05:23:19 +0200

    Hero section adjust

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,42 @@
 import type { NextPage } from "next";
+import { FC, Suspense, useEffect } from "react";
+import Button from "../components/Button";
+import LoadingSpinner from "../components/LoadingSpinner";
+
+const promise = new Promise((r) => {
+  setTimeout(() => {
+    // r();/
+  }, 2000);
+});
+
+const Component: FC = () => {
+  useEffect(() => {
+    //   setTimeout(() => {
+    // throw promise;
+    //   }, 0);
+  }, []);
+  // const load = () => {
+  if (true) {
+    // throw promise;
+  }
+  // };
+  return (
+    <>
+      <h1 className="pt-5" style={{ textAlign: "center" }}>
+        Hello from component
+      </h1>
+      <Button tag="button">Set Load</Button>
+    </>
+  );
+};
 
 const about: NextPage = () => {
   return (
     <div>
       <main className="__root">
-        <h1>About</h1>
+        {/* <Suspense fallback={<LoadingSpinner />}> */}
+        <Component />
+        {/* </Suspense> */}
       </main>
     </div>
   );
